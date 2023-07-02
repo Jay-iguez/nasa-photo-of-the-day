09c37b18092f3d2dad243c8a8dbfaab4a0d991b9
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-07-02 00:48:25 -0700

    Create useEffect calls for two slices of state/CSS style modifications.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import "./App.css";
 import MainPhoto from "./MainPhoto";
 
 function App() {
-  
+  const [nasaData, setNasaData] = useState([])
+  const [dogData, setDogData] = useState([])
+
+  useEffect(() => {
+    axios.get("https://dog.ceo/api/breeds/image/random")
+    .then(res => {
+      //console.log(res)
+      setNasaData(res.data)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  },[])
+
+  useEffect(() => {
+    axios.get("https://dog.ceo/api/breeds/image/random")
+    .then(res => {
+      //console.log(res)
+      setDogData(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  },[])
+
+  console.log(nasaData, dogData)
+
   return (
     <>
     <div className="App">
