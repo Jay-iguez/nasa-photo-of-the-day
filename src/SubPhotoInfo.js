469d065e0a6323b54fcf39eb0a9b4b98e267c271
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-07-03 19:41:47 -0700

    Add finishing touches to CSS and JSX

--- src/SubPhotoInfo.js
+++ src/SubPhotoInfo.js
@@ -1,8 +1,8 @@
 export default function SubPhotoInfo(props) {
     return (
         <div id="subPhotoInfo">
-            <p id="subPhotoTitle">Title: {props.nasaData.title}</p>
-            <p id="subPhotoExplanation">Explanation: {props.nasaData.explanation}</p>
+            <p id="subPhotoTitle"><span style={{ fontWeight: 'bold', fontSize: '4rem'}}>Title: </span>{props.nasaData.title}</p>
+            <p id="subPhotoExplanation"><span style={{ fontWeight: 'bold', fontSize: '4rem'}}>Explanation: </span>{props.nasaData.explanation}</p>
         </div>
     )
 }
